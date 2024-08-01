ba7a112d79334e0a2fdc40cc24293de6bf44634b
mohammad <mohammadghiasi005@gmail.com>
2024-08-01 07:38:15 +0330

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["vite.svg", "robots.txt", "apple-touch-icon.png"],
+      manifest: {
+        name: "Ghiasi PWA",
+        short_name: "PWA App",
+        description: "My awesome Progressive Web App!",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "vite.svg",
+            sizes: "192x192",
+            type: "image/svg+xml",
+          },
+          {
+            src: "vite.svg",
+            sizes: "512x512",
+            type: "image/svg+xml",
+          },
+        ],
+      },
+    }),
+  ],
+});
